054760f0598597d0408340f3c0151c150cc82a06
Kyamajp <94099791+Kyamajp@users.noreply.github.com>
2026-04-22 19:16:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,15 @@
-var CACHE_NAME = 'niuniu-v6';
-var urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+const CACHE_NAME="niuniu-v38";
+const ASSETS=["./","./index.html","./manifest.json","./icons/icon-192x192.png","./icons/icon-512x512.png","./icons/icon-180x180.png","./icons/icon-maskable-512x512.png"];
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(ASSETS);}));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(names) {
-      return Promise.all(
-        names.filter(function(name) { return name !== CACHE_NAME; })
-          .map(function(name) { return caches.delete(name); })
-      );
-    })
-  );
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){if(k!==CACHE_NAME)return caches.delete(k);}));
+  }));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) return response;
-      return fetch(event.request).then(function(response) {
-        if (!response || response.status !== 200 || response.type !== 'basic') return response;
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      });
-    })
-  );
+self.addEventListener("fetch",function(e){
+  e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request).then(function(r){return r||caches.match("./index.html");});}));
 });
